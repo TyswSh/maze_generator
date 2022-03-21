f4ed4fb8086abc68dc4f0ca9af67c71c852f2fa1
ShuheiT <amaleg77@gmail.com>
2022-03-21 15:57:55 +0900

    init maze

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
 }
+
+mod digging_method {
+    fn setup() {
+        let maze_width = 7;
+        let maze_height = 5;
+
+        let mut maze: Vec<Vec<char>> = Vec::new();
+
+        // #: wall, .: path
+        for _ in 0..maze_height {
+            let mut line: Vec<char> = Vec::new();
+
+            for _ in 0..maze_width {
+                line.push('#');
+            }
+
+            maze.push(line);
+        }
+    }
+}
