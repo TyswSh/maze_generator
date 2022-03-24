6bdac05715ed15709f6cce9f1c56b09e2241bdf6
ShuheiT <amaleg77@gmail.com>
2022-03-24 16:53:07 +0900

    make sturct and implement

--- src/maze_digger_generator.rs
+++ src/maze_digger_generator.rs
@@ -1,4 +1,52 @@
 use rand::{seq::SliceRandom, thread_rng, Rng};
+use std::option::Option;
+
+#[derive(Debug)]
+struct DiggerMethod {
+    width: usize,
+    height: usize,
+    path: char,
+    wall: char,
+    debug_printer: Option<bool>,
+}
+
+impl DiggerMethod {
+    pub fn new(
+        width: usize,
+        height: usize,
+        path: char,
+        wall: char,
+        is_printer: Option<bool>,
+    ) -> DiggerMethod {
+        let is_printer = is_printer.unwrap_or(false);
+        DiggerMethod {
+            width,
+            height,
+            path,
+            wall,
+            debug_printer: Some(is_printer),
+        }
+    }
+
+    pub fn println_params(&self) {
+        println!(
+            "width: {}, height: {}\npath: {}, wall: {}",
+            self.width, self.height, self.path, self.wall
+        );
+    }
+}
+
+impl Default for DiggerMethod {
+    fn default() -> Self {
+        DiggerMethod {
+            width: 13,
+            height: 13,
+            path: '.',
+            wall: '#',
+            debug_printer: Some(false),
+        }
+    }
+}
 
 pub fn generate() -> Vec<Vec<char>> {
     let maze_width = 13;
@@ -6,7 +54,6 @@ pub fn generate() -> Vec<Vec<char>> {
 
     let mut maze: Vec<Vec<char>> = Vec::new();
 
-    // #: wall, .: path
     for _ in 0..maze_height {
         let mut line: Vec<char> = Vec::new();
         for _ in 0..maze_width {
@@ -63,7 +110,6 @@ fn digger(y: usize, x: usize, maze: &mut Vec<Vec<char>>) -> Vec<Vec<char>> {
     for direction in directions {
         match direction {
             Directions::Up => {
-                // println!("Up");
                 // y-1, y-2
                 if let Some(step_y) = y.checked_sub(2) {
                     if can_dig(step_y, x, maze) {
@@ -73,7 +119,6 @@ fn digger(y: usize, x: usize, maze: &mut Vec<Vec<char>>) -> Vec<Vec<char>> {
                 }
             }
             Directions::Down => {
-                // println!("Down");
                 // y+1, y+2
                 if can_dig(y + 2, x, maze) {
                     maze[y + 1][x] = ' ';
@@ -81,7 +126,6 @@ fn digger(y: usize, x: usize, maze: &mut Vec<Vec<char>>) -> Vec<Vec<char>> {
                 }
             }
             Directions::Left => {
-                // println!("Left");
                 // x-1, x-2
                 if let Some(step_x) = x.checked_sub(2) {
                     if can_dig(y, step_x, maze) {
@@ -91,7 +135,6 @@ fn digger(y: usize, x: usize, maze: &mut Vec<Vec<char>>) -> Vec<Vec<char>> {
                 }
             }
             Directions::Right => {
-                // println!("Right");
                 // x+1, x+2
                 if can_dig(y, x + 2, maze) {
                     maze[y][x + 1] = ' ';
@@ -99,7 +142,7 @@ fn digger(y: usize, x: usize, maze: &mut Vec<Vec<char>>) -> Vec<Vec<char>> {
                 }
             }
         }
-        debug_printer(maze.to_vec());
+        // inspect_maze(maze.to_vec());
     }
     updated_maze
 }
@@ -124,7 +167,7 @@ fn can_dig(dy: usize, dx: usize, maze: &Vec<Vec<char>>) -> bool {
     };
 }
 
-fn debug_printer(maze: Vec<Vec<char>>) {
+fn inspect_maze(maze: Vec<Vec<char>>) {
     for i in maze {
         println!("{}", i.into_iter().collect::<String>());
     }
