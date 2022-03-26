7b8ba3fad04ee6f22e70efacb9cc4e1c71298a22
ShuheiT <amaleg77@gmail.com>
2022-03-26 14:17:38 +0900

    add generate

--- src/bou_taoshi_generator.rs
+++ src/bou_taoshi_generator.rs
@@ -10,33 +10,50 @@ struct MazeGrid {
 
 impl MazeGrid {
     fn new(h: usize, w: usize, path: char, wall: char) -> MazeGrid {
-        let mut gird: Vec<Vec<char>> = Vec::new();
+        let mut init_grid: Vec<Vec<char>> = vec![vec![path; w]; h];
         for i in 0..h {
             for j in 0..w {
-                if i == 0 || i == h - 1 || j == 0 || j == w - 1 || (i % 2 != 0 && j % 2 != 0) {
-                    grid[i][j] = wall;
-                } else {
-                    grid[i][j] = path;
+                if i == 0 || i == h - 1 || j == 0 || j == w - 1 || (i % 2 == 0 && j % 2 == 0) {
+                    init_grid[i][j] = wall;
                 }
             }
         }
-        MazeGrid { grid, path }
+        MazeGrid {
+            grid: init_grid,
+            path,
+        }
     }
 
     fn inspect_grid(&self) {
-        self.grid.iter().for_each(|x| pritln!("{:?}", x));
+        for i in &self.grid {
+            println!("{}", i.iter().collect::<String>());
+        }
     }
+}
+
+#[derive(Debug)]
+enum Directions {
+    Up,
+    Down,
+    Left,
+    Right,
 }
 
 #[derive(Debug)]
 pub(crate) struct BouTaoshiMethod {
     width: usize,
-    heiht: usize,
+    height: usize,
     maze: MazeGrid,
 }
 
 impl BouTaoshiMethod {
-    pub fn new(width: usize, height: usize, path: char, wall: char) -> BouTaoshiMethod {
+    pub fn new(height: usize, width: usize, path: char, wall: char) -> BouTaoshiMethod {
+        if height < 5 || width < 5 {
+            panic!(
+                "Invalid value: height = {} or width = {} are 5 or less!!",
+                height, width
+            );
+        }
         BouTaoshiMethod {
             width,
             height,
@@ -44,7 +61,57 @@ impl BouTaoshiMethod {
         }
     }
 
-    pub fn inspect_maze(&self) {
-        self.inspect_grid();
+    pub fn generate(&mut self) {
+        let y = get_random_position(&self.height);
+        let x = get_random_position(&self.width);
+
+        for i in (0..self.height).step_by(2) {
+            for j in (0..self.width).step_by(2) {
+                if i == 0 || i == self.height || j == 0 || j == self.width {
+                    println!("y:{}, x:{}", i, j);
+                }
+            }
+        }
     }
+
+    fn topple(&mut self, y: usize, x: usize) {
+        let mut directions: Vec<Directions> = vec![
+            Directions::Up,
+            Directions::Down,
+            Directions::Left,
+            Directions::Right,
+        ];
+
+        let mut rng = thread_rng();
+        directions.shuffle(&mut rng);
+
+        // 3行目以降は上を選択しない，壁がある場所は壁を選択しない
+        for direction in directions {}
+    }
+
+    pub fn inspect_maze(&self) {
+        self.maze.inspect_grid();
+    }
+}
+
+fn get_random_position(length: &usize) -> usize {
+    let mut rng = thread_rng();
+    let pos: usize = {
+        if length % 2 == 0 {
+            loop {
+                let n: usize = rng.gen_range(0..*length);
+                if n % 2 == 0 {
+                    break n;
+                }
+            }
+        } else {
+            loop {
+                let n = rng.gen_range(0..*length) as usize;
+                if n % 2 != 0 {
+                    break n;
+                }
+            }
+        }
+    };
+    pos
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 mod bou_taoshi_generator;
-mod maze_digger_generator;
+// mod maze_digger_generator;
 
 fn main() {
     // height and width must be greater than 2.
-    let mut mdm = maze_digger_generator::DiggerMethod::new(7, 7, '.', '#');
-    mdm.generate();
+    // let mut mdm = maze_digger_generator::DiggerMethod::new(7, 7, '.', '#');
+    // mdm.generate();
 
     // only print in console
-    mdm.inspect_maze();
+    // mdm.inspect_maze();
 
     // return vec<vec<char>> of maze grid
-    mdm.get_maze_grid();
+    // mdm.get_maze_grid();
 
     // panic
     // let mut mdm = maze_digger_generator::DiggerMethod::new(2, 2, 'O', 'X');
 
-    let btm = bou_taoshi_generator::BouTaoshiMethod::new(5, 5, '.', '#');
-    btm.inspect_maze();
+    let mut btm = bou_taoshi_generator::BouTaoshiMethod::new(5, 5, '.', '#');
+    // btm.inspect_maze();
+    btm.generate();
 }
